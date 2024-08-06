653e84c765d2d0c58e910524e3157cfa40df443b
anoying-kid <yugrana853@gmail.com>
2024-08-06 13:23:08 +0530

    Added token refresh url

--- user_login/urls.py
+++ user_login/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import user_signup,celebrity_signup,user_login, celebrity_login
 
 urlpatterns = [
     path('signup/',user_signup,name='user-signup'),
-    path('celebrity/signup',celebrity_signup,name='celebrity-signup'),
+    path('celebrity/signup/',celebrity_signup,name='celebrity-signup'),
     path('login/', user_login, name='user-login'),
     path('celebrity/login/', celebrity_login, name='celebrity-login'),
-    
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh')
 ]
